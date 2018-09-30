1d95794927690096bff9246428a4d2e14a15fd51
Akash Chandra <akash.chandra8d@gmail.com>
2018-09-30 22:06:48 +0530

    Updated

--- app.py
+++ app.py
@@ -37,8 +37,8 @@ def chat():
 
 
 if __name__ == "__main__":
-	# port = int(os.environ['PORT'])
-	port = 8080
+	port = int(os.environ['PORT'])
+	# port = 8080
 	app.run(host='localhost', port = port, debug = True, threaded = True)
 	# text = 'Show me a veg pizza'
 	# nlu = NLU()
