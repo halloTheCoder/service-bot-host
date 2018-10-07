79db09bfec82049369f1d031148c49f5ee083cb4
Akash Chandra <akash.chandra8d@gmail.com>
2018-10-07 20:33:55 +0530

    udpated

--- app.py
+++ app.py
@@ -9,7 +9,12 @@ import numpy as np
 import json
 import os
 
-from dialogue_management_model import run_service_bot, dialogue_initialization
+# from dialogue_management_model import run_service_bot, dialogue_initialization
+
+import rasa_core
+from rasa_core.agent import Agent
+from rasa_core.interpreter import RasaNLUInterpreter
+from rasa_core.utils import EndpointConfig
 
 app = Flask(__name__)
 app.secret_key = '12345'
@@ -19,7 +24,16 @@ agent = None
 @app.route('/')
 def hello_world():
 	global agent
-	agent = dialogue_initialization()
+
+	print('Loading Model')
+
+	interpreter = RasaNLUInterpreter('./models/nlu_tf/default/service_nlu')
+	action_endpoint = EndpointConfig(url = "http://localhost:5055/webhook")
+	
+	agent = Agent.load('./models/dialogue', interpreter = interpreter, action_endpoint = action_endpoint)
+	
+	print('Model loaded')
+
 	return render_template('home.html')
 
 
@@ -30,13 +44,24 @@ def chat():
 	print(type(user_message))
 	
 	global agent
-	response = run_service_bot(user_message, agent)
-	print(response)
+
+	responses = agent.handle_text(user_message)
+	print(responses)
+
+	ans = ""
+
+	for i in range(len(responses)):
+		if 'recipient_id' in responses[i]:
+			ans += responses[i]['text']
+			if not i == (len(responses) - 1):
+				ans += '\n'
 	
-	return jsonify({"status":"success","response":str(response)})
+	print(ans)
+
+	return jsonify({"status":"success","response":str(ans)})
 
 
 if __name__ == "__main__":
 	# port = int(os.environ['PORT'])
-	port = 8889
+	port = 8888
 	app.run(host='localhost', port = port, debug = True)
